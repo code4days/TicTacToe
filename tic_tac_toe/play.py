a377a9ca6ffdc3a4dc5b52ed1b899068f2586f91
Rasheed E <relsaleh@gmail.com>
2023-06-28 23:22:41 +0000

    add some UI functionality

--- tic_tac_toe/play.py
+++ tic_tac_toe/play.py
@@ -5,23 +5,11 @@ from textual.app import App, ComposeResult
 from textual.widget import Widget
 from textual.widgets import Button, Footer, Header, Label
 
-
-# q: are you familiar with textual and how it works?
-
-
+from tic_tac_toe.tic_tac_toe_game import TicTacToeGame
 
 """
 This is a simple Tic Tac Toe game with a Textual interface.
 It is a work in progress.
-
-TODO:
-Write code to play game.
-Write code to determine winner.
-Write code to reset game.
-Write code to play again.
-Write code to play against computer.
-Write code to play against another player.
-Handle UI events.
 """
 
 
@@ -54,6 +42,10 @@ class TicTacToeApp(App):
 
     SIZE: Final = 3
 
+    def __init__(self) -> None:
+        super().__init__()
+        self.game = TicTacToeGame()
+
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
 
@@ -61,24 +53,45 @@ class TicTacToeApp(App):
         yield Footer()
         yield Label("Play Tic Tac Toe")
         yield GameGrid()
-        yield Label("Winner:")
+        yield Label(id="winner-label")
 
     def action_toggle_dark(self) -> None:
         self.dark = not self.dark
 
-    
+    def _update_button(self, button: GameCell, player: str):
+        """Update button with player's symbol"""
+
+        button.label = player
+        button.disabled = True
+        button.classes = player
+
     @on(Button.Pressed)
     def handle_button_pressed(self, message: Button.Pressed):
         """Handle a button pressed event"""
-        
+        print(message.button)
+        row, col = message.button.row, message.button.col
+        print(f"Row: {row}, Col: {col}")
         print(f"Button {message.button.id} pressed")
-        message.button.label = "X"
-        message.button.disabled = True
-        message.button.classes = "x"
 
+        if self.game.is_valid_move(row, col):
+            self._update_button(message.button, self.game.player)
+            self.game.update_board(row, col)
 
+            if self.game.has_winner(player=self.game.player, row=row, col=col):
+                msg = f"Player {self.game.player} wins!"
+                winner_label = self.query_one("#winner-label", Label)
+                winner_label.add_class("visible")
+                winner_label.update(msg)
+                return
 
-    
+            if self.game.is_board_full():
+                msg = "Game Tied"
+
+                winner_label = self.query_one("#winner-label", Label)
+                winner_label.update(msg)
+                winner_label.add_class("visible")
+                return
+        self.game.switch_players()
 
 
 if __name__ == "__main__":
---
+++ tic_tac_toe/tic_tac_toe_game.py
@@ -0,0 +1,99 @@
+"""
+Create a playable Tic Tac Toe game
+
+two players, x and o
+x goes first
+x and o take turns
+
+winner is first player to get three in a row, a column, diagonal, or anti-diagonal
+
+get move, check if valid, check if move wins, repeat until winner or board full
+
+"""
+
+
+# create game logic class
+
+BOARD_SIZE = 3
+FIRST_PLAYER = "X"
+
+
+class TicTacToeGame:
+    def __init__(self):
+        self.player = FIRST_PLAYER
+        self.winner = ""
+        self.move_count = 0
+        self.board = self.create_board()
+
+    def create_board(self) -> list[list[str]]:
+        """Create a new board"""
+
+        return [["" for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+
+    def is_valid_move(self, row: int, col: int) -> bool:
+        """Check if move is valid"""
+        # TODO: check if we have a winner?
+        if self.board[row][col]:
+            return False
+        self.move_count += 1
+        return True
+
+    def is_board_full(self) -> bool:
+        """Check if board is full"""
+
+        if self.move_count == BOARD_SIZE * BOARD_SIZE:
+            return True
+        return False
+
+    def check_row(self, player: str, row: int) -> bool:
+        """Check for winner in row"""
+
+        for col in range(BOARD_SIZE):
+            if self.board[row][col] != player:
+                return False
+        return True
+
+    def check_column(self, player: str, col: int) -> bool:
+        """Check for winner in column"""
+
+        for row in range(BOARD_SIZE):
+            if self.board[row][col] != player:
+                return False
+        return True
+
+    def check_diagonal(self, player: str) -> bool:
+        """Check for winner in diagonal"""
+
+        for row in range(BOARD_SIZE):
+            if self.board[row][row] != player:
+                return False
+        return True
+
+    def check_anti_diagonal(self, player: str) -> bool:
+        """Check for winner in anti-diagonal"""
+
+        for row in range(BOARD_SIZE):
+            col = BOARD_SIZE - row - 1
+            if self.board[row][col] != player:
+                return False
+        return True
+
+    def has_winner(self, player: str, row: int, col: int) -> bool:
+        """Determine if current move is a winning move"""
+
+        return any(
+            [
+                self.check_row(player, row),
+                self.check_column(player, col),
+                self.check_diagonal(player),
+                self.check_anti_diagonal(player),
+            ]
+        )
+
+    def update_board(self, row: int, col: int) -> None:
+        """Update board with player's move"""
+
+        self.board[row][col] = self.player
+
+    def switch_players(self) -> None:
+        self.player = "X" if self.player == "O" else "O"
